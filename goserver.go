787bdb9ad123c75de468734609a9877a4fbeb903
Unknown0156 <paxando@gmail.com>
2021-04-27 18:22:23 +0300

    simple shell

--- goserver.go
+++ goserver.go
@@ -6,20 +6,21 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"regexp"
 	"strings"
+	"time"
 )
+
+var templates = template.Must(template.ParseFiles("templates/view.html", "templates/edit.html"))
+var validPath = regexp.MustCompile("^/(edit/|save/|)([a-zA-Z0-9]+)$")
 
 type Page struct {
 	Title string
 	Body  []byte
 }
-
-var templates = template.Must(template.ParseFiles("templates/view.html", "templates/edit.html"))
-var validPath = regexp.MustCompile("^/(edit/|save/|)([a-zA-Z0-9]+)$")
 
 func (p *Page) save() error {
 	filename := p.Title + ".html"
@@ -35,27 +36,23 @@ func (p *Page) load(title string) error {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
+	if err := templates.ExecuteTemplate(w, tmpl+".html", p); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request, title string) {
 	var p Page
-	err := p.load(title)
-	if err != nil {
+	if err := p.load(title); err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
 	w.Write(p.Body)
-	//renderTemplate(w, "view", &p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	var p Page
-	err := p.load(title)
-	if err != nil {
+	if err := p.load(title); err != nil {
 		p = Page{Title: title}
 	}
 	renderTemplate(w, "edit", &p)
@@ -64,8 +61,7 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
+	if err := p.save(); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -87,57 +83,92 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
-func execInput(input string) error {
-	input = strings.TrimSuffix(input, "\n")
-	cmd := exec.Command(input)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
+func startServer(logger *log.Logger, addr string, srvch chan string) *http.Server {
+	srv := &http.Server{Addr: addr}
+	go func() {
+		defer func() {
+			srvch <- srv.Addr
+		}()
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			logger.Fatalf("ListenAndServe (%s): %v", srv.Addr, err)
+		} else {
+			logger.Printf("Server %s shuting down", srv.Addr)
+		}
+	}()
+	logger.Printf("Server %s started", srv.Addr)
+	return srv
 }
 
 func main() {
-	var srv http.Server
-	srv.Addr = ":80"
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	srvch := make(chan string)
+	srvmap := make(map[string]*http.Server)
+	srvmap[":80"] = startServer(logger, ":80", srvch)
 	http.HandleFunc("/", makeHandler(mainHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
-	var err error
-	go func() {
-		err = srv.ListenAndServe()
-	}()
-	reader := bufio.NewReader(os.Stdin)
+
+	//Server shell
 	fmt.Println("GoServer Shell")
 	fmt.Println("---------------------")
-	fmt.Println("Server started")
+	time.Sleep(1 * time.Second)
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("-> ")
+		select {
+		case srvaddr := <-srvch:
+			_, ok := srvmap[srvaddr]
+			if ok {
+				delete(srvmap, srvaddr)
+			} else {
+				logger.Panicf("No %s in server's map", srvaddr)
+			}
+			continue
+		default:
+		}
+		fmt.Print(">")
 		input, err := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\n")
-		input = strings.TrimSuffix(input, "\r")
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
+			continue
 		}
+		input = strings.TrimSuffix(input, "\n")
+		input = strings.TrimSuffix(input, "\r")
 		if input == "exit" {
+			for _, srv := range srvmap {
+				srv.Shutdown(context.Background())
+			}
 			break
 		}
 		switch input {
+		case "help":
+			fmt.Println("Commands: start, stop, status, exit")
 		case "start":
-			go func() {
-				err = srv.ListenAndServe()
-			}()
-			fmt.Println("Server started")
-		case "shutdown":
-			srv.Shutdown(context.Background())
-			fmt.Println("Server shutdown")
+			newaddr := ":80"
+			_, ok := srvmap[newaddr]
+			if ok {
+				fmt.Println("Server is already running!")
+			} else {
+				srvmap[newaddr] = startServer(logger, newaddr, srvch)
+			}
+		case "stop":
+			stopaddr := ":80"
+			srv, ok := srvmap[stopaddr]
+			if ok {
+				srv.Shutdown(context.Background())
+			} else {
+				fmt.Println("No such server running!")
+			}
+		case "status":
+			for addr, _ := range srvmap {
+				fmt.Printf("Server %s is runing\n", addr)
+			}
 		default:
-			fmt.Println("No such command")
+			fmt.Println("No such command, use 'help'")
 		}
 	}
-	//log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 /*TODO
-Terminate command
 TestLoad css
 TestLoad Image
 Edit btn
